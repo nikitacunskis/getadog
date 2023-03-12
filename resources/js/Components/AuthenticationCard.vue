0f3cfc1e4227c5441f68e130d22a809a4c8f042b
Nikita Cunskis <nikita.cunskis@gmail.com>
2023-03-12 21:05:08 +0200

    initial commit

---
+++ resources/js/Components/AuthenticationCard.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="min-h-screen flex flex-col sm:justify-center sm:pt-0">    
+        <div class="w-full sm:max-w-md mt-6 px-6 shadow-md overflow-hidden sm:rounded-lg">
+            <slot />
+        </div>
+    </div>
+</template>
